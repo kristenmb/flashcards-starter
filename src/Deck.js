4db1a4933f9642890de52c199a0bbd7b52eb0fe6
Kristen Bair <kristenmbair@gmail.com>
2020-11-30 19:39:04 -0700

    Refactor to utilize ES6 syntax

--- src/Deck.js
+++ src/Deck.js
@@ -2,14 +2,10 @@ class Deck {
   constructor(cards) {
     this.cards = cards;
   }
-  
+
   countCards() {
     return this.cards.length
   }
 }
 
 module.exports = Deck;
-
-
-
-//Your Deck class should be initialized with an array of Card objects and should have an accompanying test file. It should know how many Cards are in the Deck.
