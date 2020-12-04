7b5c1b909cfda2246e881a9840534671914971a6
Kristen Bair <kristenmbair@gmail.com>
2020-12-03 17:07:49 -0700

    Remove uneccesary spacing

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const Game = require('./src/Game');
 app.listen(3000, '127.0.0.1');
 console.log('Node server running on port 3000');
 
-let game = new Game();
+const game = new Game();
 game.start();
